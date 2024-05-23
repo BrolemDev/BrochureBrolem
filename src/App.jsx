afa1643a122fdb19f8f8666391af7e4e245b1345
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2024-05-23 00:26:04 -0500

    :rocket: Added Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Preloader from './components/Preloader';
 import PDFViewer from './components/PDFViewer';
 import { updateMetaTags } from './utils/updateMetaTags';
+
 function App() {
   const [loading, setLoading] = useState(true);
 
@@ -19,15 +20,19 @@ function App() {
   return (
     <Router>
       <div className="h-screen w-screen">
-        {loading ? <Preloader /> : (
+        {loading ? (
+          <Preloader />
+        ) : (
           <Routes>
             <Route path="/en" element={<PDFViewer language="en" />} />
             <Route path="/ko" element={<PDFViewer language="ko" />} />
+            <Route path="/*" element={<Navigate to="/en" replace />} />
           </Routes>
         )}
       </div>
     </Router>
   );
 }
+
 
 export default App;
