38e2af03ba3b50b137398ba747855f7d8cd62362
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2024-05-22 23:37:06 -0500

    :rocket: Added library router react

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Preloader from './components/Preloader';
 import PDFViewer from './components/PDFViewer';
 
-function getLanguageFromURL() {
-  // Obtener el idioma de la URL (por ejemplo, /es o /en)
-  const path = window.location.pathname;
-  const language = path.substring(1); // Eliminar la barra inicial
-  return language;
-}
-
 function App() {
   const [loading, setLoading] = useState(true);
-  const [language, setLanguage] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
     }, 1500);
-    setLanguage(getLanguageFromURL()); // Obtener el idioma al cargar la aplicación
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="h-screen w-screen">
-      {loading ? <Preloader /> : <PDFViewer language={language} />}
-    </div>
+    <Router>
+      <div className="h-screen w-screen">
+        {loading ? <Preloader /> : (
+          <Routes>
+            <Route path="/en" element={<PDFViewer language="en" />} />
+            <Route path="/ko" element={<PDFViewer language="ko" />} />
+            {/* Otras rutas */}
+          </Routes>
+        )}
+      </div>
+    </Router>
   );
 }
 
